f63036062e5890ac8e68b0bc4d0fbdd366ec9e6a
Jang Oo Bin <jwb449730@naver.com>
2021-10-18 18:11:05 +0900

    2021_10_18

--- sec05/5_04.js
+++ sec05/5_04.js
@@ -1,7 +1,15 @@
 //연속 부분수열 2
 function solution(m, arr) {
-  let answer = 0,
-  
+  let answer = 0;
+  let lp = 0;
+  let sum = 0;
+  for (let rp = 0; rp < arr.length; rp++) {
+    sum += arr[rp];
+    while (sum > m) {
+      sum -= arr[lp++];
+    }
+    answer += rp - lp + 1; // 자기자신을 포함한 연속부분 수열
+  }
   return answer;
 }
 

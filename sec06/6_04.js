71d54ad9653ff2d297760b8243dbe2014b41c4d1
Jang Oo Bin <jwb449730@naver.com>
2021-10-24 09:56:54 +0900

    2021_10_24

--- sec06/6_04.js
+++ sec06/6_04.js
@@ -1,9 +1,66 @@
 //후위식 연산(postfix)
 function solution(s) {
-  let answer;
+  let answer = 0;
+  let stack = [];
+  let cnt = true;
+
+  for (let x of s) {
+    if (Number(x)) {
+      stack.push(Number(x));
+    } else {
+      if (cnt) {
+        if (x === "+") {
+          answer += stack[stack.length - 1] + stack[stack.length - 2];
+        } else if (x === "-") {
+          answer += stack[stack.length - 1] - stack[stack.length - 2];
+        } else if (x === "*") {
+          answer += stack[stack.length - 1] * stack[stack.length - 2];
+        } else if (x === "/") {
+          answer += stack[stack.length - 1] / stack[stack.length - 2];
+        }
+        stack.pop();
+        stack.pop();
+        cnt = false;
+      } else {
+        if (x === "+") {
+          answer += stack[stack.length - 1];
+        } else if (x === "-") {
+          answer -= stack[stack.length - 1];
+        } else if (x === "/") {
+          answer /= stack[stack.length - 1];
+        } else if (x === "*") {
+          answer *= stack[stack.length - 1];
+        }
+        stack.pop();
+      }
+    }
+  }
 
   return answer;
 }
 
 let str = "352+*9-";
-console.log(solution(str));
+//console.log(solution(str));
+
+//후위식 연산(postfix)
+function solution2(s) {
+  let answer = 0;
+  let stack = [];
+  for (let x of s) {
+    if (!isNaN(x)) stack.push(Number(x));
+    else {
+      let rt = stack.pop();
+      let lt = stack.pop();
+      if (x === "+") stack.push(lt + rt);
+      else if (x === "-") stack.push(lt - rt);
+      else if (x === "*") stack.push(lt * rt);
+      else if (x === "/") stack.push(lt / rt);
+    }
+  }
+  answer = stack[0];
+
+  return answer;
+}
+
+let str2 = "352+*9-";
+console.log("solution2 = " + solution2(str2));

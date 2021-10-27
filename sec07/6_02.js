29591d06e0255e11cd02fa23aed057bafb254b72
Jang Oo Bin <jwb449730@naver.com>
2021-10-27 15:30:08 +0900

    2021_10_27

--- sec07/6_02.js
+++ sec07/6_02.js
@@ -1,8 +1,19 @@
 //버블 정렬
 function solution(arr) {
-  let answer;
+  let answer = arr;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let idx = 0;
+    for (let j = idx + 1; j < arr.length - i; j++) {
+      if (arr[idx] > arr[j]) {
+        [arr[idx], arr[j]] = [arr[j], arr[idx]];
+      }
+      idx++;
+    }
+    console.log(arr);
+  }
+
   return answer;
 }
-
+let a = [7, 54, 3, 8, 12, 4, 83, 9, 10];
 let arr = [13, 5, 11, 7, 23, 15];
-console.log(solution(arr));
+console.log(solution(a));

5a14a7aa0f5e9be784841067b2219463c31daae5
Jang Oo Bin <jwb449730@naver.com>
2021-10-29 20:01:06 +0900

    2021_10_29

--- sec07/7_04.js
+++ sec07/7_04.js
@@ -1,8 +1,48 @@
 // 삽입 정렬
 function solution(arr) {
-  let answer;
+  let answer = arr;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (idx === 0) {
+        if (arr[idx] > arr[j]) {
+          [arr[idx], arr[j]] = [arr[j], arr[idx]];
+        }
+        break;
+      } else {
+        if (arr[idx] > arr[j]) {
+          while (idx >= 0) {
+            if (arr[idx] > arr[j]) {
+              [arr[idx], arr[j]] = [arr[j], arr[idx]];
+              idx--;
+              j--;
+            } else break;
+          }
+        } else break;
+      }
+    }
+  }
+
   return answer;
 }
 
 let arr = [11, 7, 5, 6, 10, 9];
 console.log(solution(arr));
+
+// 삽입 정렬
+function solution2(arr2) {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = arr[i];
+    let j;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > tmp) arr[j + 1] = arr[j];
+      else break;
+    }
+    arr[j + 1] = tmp;
+  }
+  return answer;
+}
+
+let arr2 = [11, 7, 5, 6, 10, 9];
+console.log(solution2(arr2));

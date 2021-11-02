b3707efd24b1c2b8be87c7d6c4ae299f51cf322c
Jang Oo Bin <jwb449730@naver.com>
2021-11-02 15:01:01 +0900

    2021_11_02

--- sec07/7_07.js
+++ sec07/7_07.js
@@ -1,6 +1,17 @@
 // 좌표 정렬
 function solution(arr) {
-  let answer;
+  let answer = arr;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i][0] > arr[i + 1][0]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
+  }
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i][0] == arr[i + 1][0] && arr[i][1] > arr[i + 1][1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
+  }
   return answer;
 }
 
@@ -12,3 +23,26 @@ let arr = [
   [3, 6],
 ];
 console.log(solution(arr));
+
+// 좌표 정렬
+function solution2(arr) {
+  let answer = arr;
+
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    } else {
+      return a[0] - b[0];
+    }
+  });
+  return answer;
+}
+
+let arr2 = [
+  [2, 7],
+  [1, 3],
+  [1, 2],
+  [2, 5],
+  [3, 6],
+];
+console.log(solution2(arr2));

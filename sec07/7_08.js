1ff2f067c129c42b6c47f84d437ebab0ad829482
Jang Oo Bin <jwb449730@naver.com>
2021-11-03 17:06:42 +0900

    2021_11_03

--- sec07/7_08.js
+++ sec07/7_08.js
@@ -1,14 +1,28 @@
 // 회의실 배정
 function solution(meeting) {
-  let answer;
+  let answer = 0;
+  meeting.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  console.log(meeting);
+  for (let i = 0; i < meeting.length - 1; i++) {
+    if (meeting[i][1] === meeting[i + 1][0]) {
+      answer++;
+    }
+  }
   return answer;
 }
-
+//01 10 ] 11 20 ] 21 30
 let arr = [
   [1, 4],
   [2, 3],
   [3, 5],
   [4, 6],
   [5, 7],
+];
+let arr2 = [
+  [3, 3],
+  [1, 3],
+  [2, 3],
 ];
 console.log(solution(arr));

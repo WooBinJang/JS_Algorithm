cc177adc2c5e48994050bf83d078ffb880f384db
Jang Oo Bin <jwb449730@naver.com>
2021-11-04 16:56:19 +0900

    2021_11_04

--- sec07/7_08.js
+++ sec07/7_08.js
@@ -1,15 +1,22 @@
 // 회의실 배정
 function solution(meeting) {
-  let answer = 0;
+  let answer;
+  let tmparr = [];
   meeting.sort((a, b) => {
     return a[0] - b[0];
   });
-  console.log(meeting);
-  for (let i = 0; i < meeting.length - 1; i++) {
-    if (meeting[i][1] === meeting[i + 1][0]) {
-      answer++;
+  for (let i = 0; i < meeting.length; i++) {
+    let cnt = 1;
+    let tmp = i;
+    for (let j = i + 1; j < meeting.length; j++) {
+      if (meeting[tmp][1] <= meeting[j][0]) {
+        tmp = j;
+        cnt++;
+      }
     }
+    tmparr.push(cnt);
   }
+  answer = Math.max(...tmparr);
   return answer;
 }
 //01 10 ] 11 20 ] 21 30
@@ -25,4 +32,37 @@ let arr2 = [
   [1, 3],
   [2, 3],
 ];
-console.log(solution(arr));
+console.log(solution(arr2));
+
+// 회의실 배정
+function solution2(meeting) {
+  let answer = 0;
+
+  meeting.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - b[1];
+  });
+  console.log(meeting);
+  let et = 0;
+  for (let x of meeting) {
+    if (x[0] >= et) {
+      answer++;
+      et = x[1];
+    }
+  }
+  return answer;
+}
+//01 10 ] 11 20 ] 21 30
+let arr3 = [
+  [1, 4],
+  [2, 3],
+  [3, 5],
+  [4, 6],
+  [5, 7],
+];
+let arr4 = [
+  [3, 3],
+  [1, 3],
+  [2, 3],
+];
+console.log("solution2 = " + solution2(arr3));
--- sec07/7_09.js
+++ sec07/7_09.js
@@ -1,6 +1,24 @@
 // 결혼식
 function solution(times) {
-  let answer;
+  let answer = [];
+
+  times.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    } else return a[0] - b[0];
+  });
+
+  for (let i = 0; i < times.length; i++) {
+    let cnt = 0;
+
+    for (let j = 0; j < times.length; j++) {
+      if (times[i][0] <= times[j][0] && times[i][1] > times[j][0]) {
+        cnt++;
+      }
+    }
+    answer.push(cnt);
+  }
+  answer = Math.max(...answer);
   return answer;
 }
 
@@ -11,3 +29,40 @@ let arr = [
   [20, 30],
   [5, 14],
 ];
+console.log(solution(arr));
+// 결혼식
+function solution2(times) {
+  let answer = 0;
+  let T_line = [];
+  for (let x of times) {
+    T_line.push([x[0], "s"]);
+    T_line.push([x[1], "e"]);
+  }
+  T_line.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1].charCodeAt() - b[1].charCodeAt(); // 아스키코드로 변경후 sort
+    } else {
+      return a[0] - b[0];
+    }
+  });
+
+  let cnt = 0;
+  for (let x of T_line) {
+    if (x[1] === "s") {
+      cnt++;
+    } else {
+      cnt--;
+    }
+    answer = Math.max(answer, cnt);
+  }
+  return answer;
+}
+
+let arr2 = [
+  [14, 18],
+  [12, 15],
+  [15, 20],
+  [20, 30],
+  [5, 14],
+];
+console.log(solution2(arr2));

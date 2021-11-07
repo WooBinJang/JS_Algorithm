badf23f1b8cb98188bf01113b341a2d8eadc2f3d
Jang Oo Bin <jwb449730@naver.com>
2021-11-07 14:55:06 +0900

    2021_11_07

--- sec07/7_10.js
+++ sec07/7_10.js
@@ -1,12 +1,48 @@
 // 이분검색
 function solution(target, arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(i);
-  }
   let answer = 0;
+  arr.sort((a, b) => a - b);
+  let tmp = 0;
+  let si = 0;
+  let li = arr.length - 1;
+
+  while (target !== tmp) {
+    let i = Math.floor((si + li) / 2);
+    if (arr[i] > target) {
+      li = i;
+    } else if (arr[i] < target) {
+      si = i;
+    } else if (arr[i] === target) {
+      tmp = arr[i];
+      answer = i + 1;
+    }
+  }
 
   return answer;
 }
 
 let arr = [23, 87, 65, 12, 57, 32, 99, 81];
 console.log(solution(32, arr));
+
+// 이분검색
+function solution2(target, arr) {
+  let answer = 0;
+  arr.sort((a, b) => a - b);
+  let lt = 0;
+  let rt = arr.length - 1;
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (arr[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (arr[mid] > target) {
+      rt = mid - 1;
+    } else {
+      lt = mid + 1;
+    }
+  }
+  return answer;
+}
+
+let arr2 = [23, 87, 65, 12, 57, 32, 99, 81];
+console.log("solution2 = " + solution2(32, arr2));
--- sec07/7_11.js
+++ sec07/7_11.js
@@ -1,7 +1,32 @@
 // 뮤직비디오(결정알고리즘)
 function solution(m, songs) {
-  let answer;
+  let answer = 0;
+  let sum = 0;
+  for (let x of songs) {
+    sum += x;
+  }
+  sum = Math.ceil(sum / 3);
+  let cnt = 1;
 
+  for (sum; sum++; ) {
+    let sum_tmp = sum;
+    for (let i = 0; i < songs.length; i++) {
+      if (sum - songs[i] >= 0) {
+        sum -= songs[i];
+      } else {
+        cnt++;
+        sum = sum_tmp;
+        sum -= songs[i];
+      }
+      console.log(songs[i] + "  " + sum + "  " + cnt);
+    }
+    if (cnt !== m) {
+      cnt = 1;
+    } else {
+      answer = sum;
+      break;
+    }
+  }
   return answer;
 }
 

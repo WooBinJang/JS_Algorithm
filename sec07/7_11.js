fdd1d8da15ffb394a485cf03bec9fe090dd44913
Jang Oo Bin <jwb449730@naver.com>
2021-11-08 18:51:40 +0900

    2021_11_08

--- sec07/7_11.js
+++ sec07/7_11.js
@@ -1,34 +1,61 @@
 // 뮤직비디오(결정알고리즘)
 function solution(m, songs) {
   let answer = 0;
-  let sum = 0;
-  for (let x of songs) {
-    sum += x;
-  }
-  sum = Math.ceil(sum / 3);
-  let cnt = 1;
-
-  for (sum; sum++; ) {
-    let sum_tmp = sum;
-    for (let i = 0; i < songs.length; i++) {
-      if (sum - songs[i] >= 0) {
-        sum -= songs[i];
-      } else {
+  let max = Math.max(...songs);
+  while (max) {
+    let tmp = max;
+    let cnt = 1;
+    for (let j = 0; j < songs.length; j++) {
+      if (tmp - songs[j] < 0) {
+        tmp = max;
         cnt++;
-        sum = sum_tmp;
-        sum -= songs[i];
       }
-      console.log(songs[i] + "  " + sum + "  " + cnt);
+      tmp -= songs[j];
     }
-    if (cnt !== m) {
-      cnt = 1;
-    } else {
-      answer = sum;
+    if (cnt === m) {
+      answer = max;
       break;
     }
+    max++;
   }
+
   return answer;
 }
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(solution(3, arr));
+
+// 뮤직비디오(결정알고리즘)
+
+function count(songs, mid) {
+  let cnt = 1;
+  let sum = 0;
+  for (let x of songs) {
+    if (sum + x > mid) {
+      cnt++;
+      sum = x;
+    } else {
+      sum += x;
+    }
+  }
+  return cnt;
+}
+
+function solution2(m, songs) {
+  let answer;
+  let lt = Math.max(...songs);
+  let rt = songs.reduce((a, b) => a + b, 0);
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (count(songs, mid) <= m) {
+      answer = mid;
+      rt = mid - 1;
+    } else {
+      lt = mid + 1;
+    }
+  }
+  return answer;
+}
+
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("solution2 = " + solution2(3, arr2));
--- sec07/7_12.js
+++ sec07/7_12.js
@@ -1,7 +1,31 @@
 // 마구간 정하기(결정알고리즘)
-function solution(c, stable) {
-  let answer;
+function count(arr, mid) {
+  // 말 수 검사
+  let cnt = 1;
+  let ep = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - ep >= mid) {
+      cnt++;
+      ep = arr[i];
+    }
+  }
+  return cnt;
+}
 
+function solution(c, stable) {
+  let answer = 0;
+  stable.sort((a, b) => a - b);
+  let lt = 1; // 최소 거리
+  let rt = stable[stable.length - 1] - stable[0];
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2); // 거리
+    if (count(stable, mid) >= c) {
+      answer = mid;
+      lt = mid + 1;
+    } else {
+      rt = mid - 1;
+    }
+  }
   return answer;
 }
 

0401b0c140f9b917baae69a1e119f92d60e3819f
Jang Oo Bin <jwb449730@naver.com>
2021-11-11 13:54:56 +0900

    2021_11_11

--- sec08/8_03.js
+++ sec08/8_03.js
@@ -1,7 +1,24 @@
 // 이진트리 순회(깊이우선탐색)
 function solution(n) {
   let answer = "";
-
+  let preorder = []; // 전위순회
+  let inorder = []; // 중위순회
+  let postorder = []; // 후위순회
+  function DFS(v) {
+    if (v > 7) {
+      return;
+    } else {
+      preorder.push(v);
+      DFS(v * 2); // 왼쪽 자식 노드
+      inorder.push(v);
+      DFS(v * 2 + 1); // 오른쪼 자식 노드
+      postorder.push(v);
+    }
+  }
+  DFS(n);
+  console.log("전위순회 = " + preorder);
+  console.log("중위순회 = " + inorder);
+  console.log("후위순회 = " + postorder);
   return answer;
 }
 

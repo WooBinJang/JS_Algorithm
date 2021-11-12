a7b44668c9c60c4f40933ce4735d591d52a4fafa
Jang Oo Bin <jwb449730@naver.com>
2021-11-12 14:49:45 +0900

    2021_11_11

--- sec08/8_04.js
+++ sec08/8_04.js
@@ -1,14 +1,27 @@
 // 부분집합 구하기(DFS)
 function solution(n) {
   let answer = [];
-function DFS(L){
-  if(){
+  let ch = Array.from({ length: n + 1 }, () => false);
 
-  }else{
-
+  function DFS(v) {
+    if (v === n + 1) {
+      let tmp = "";
+      for (let i = 1; i <= n; i++) {
+        if (ch[i]) tmp += i + " ";
+      }
+      if (tmp.length > 0) {
+        // 공집합 제거
+        answer.push(tmp.trim());
+      }
+    } else {
+      ch[v] = true; // 포함한다
+      DFS(v + 1);
+      ch[v] = false; // 포함하지 않는다.
+      DFS(v + 1);
+    }
   }
-  DFS(n);
-}
+  DFS(1);
+
   return answer;
 }
 

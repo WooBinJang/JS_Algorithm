19431888540f9e1ee4886e322d66b9b6ae2c22f5
Jang Oo Bin <jwb449730@naver.com>
2021-11-14 15:58:31 +0900

    2021_11_14

--- sec08/8_05.js
+++ sec08/8_05.js
@@ -1,14 +1,60 @@
 // 합이 같은 부분집합(DFS : 아마존 인터뷰)
 function solution(arr) {
-  let answer = "";
+  let answer = "NO";
+  let tmparr = Array.from({ length: arr.length }, () => false);
+
   function DFS(v) {
-    if (v === 0) {
+    if (v === arr.length) {
+      let sum1 = 0;
+      let sum2 = 0;
+
+      for (let i = 0; i < arr.length; i++) {
+        if (tmparr[i]) {
+          sum1 += arr[i];
+        } else {
+          sum2 += arr[i];
+        }
+      }
+      if (sum1 === sum2) {
+        answer = "YES";
+      }
     } else {
+      tmparr[v] = true;
+      DFS(v + 1);
+      tmparr[v] = false;
+      DFS(v + 1);
     }
   }
-  DFS(arr);
+  DFS(0);
+
   return answer;
 }
 
 let arr = [1, 3, 5, 6, 7, 10];
 console.log(solution(arr));
+
+// 합이 같은 부분집합(DFS : 아마존 인터뷰)
+function solution2(arr) {
+  let answer = "NO";
+  let total = arr.reduce((a, b) => a + b);
+  let flag = true;
+  function DFS(L, sum) {
+    if (L === arr.length) {
+      if (flag) {
+        if (total - sum === sum) {
+          answer = "YES";
+          flag = false;
+        }
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+  DFS(0, 0);
+
+  return answer;
+}
+
+let arr2 = [1, 3, 5, 6, 7, 10];
+console.log("solution2 = " + solution2(arr2));
--- sec08/8_06.js
+++ sec08/8_06.js
@@ -1,8 +1,50 @@
 // 바둑이 승차(DFS)
 function solution(c, arr) {
   let answer = 0;
+  let tmparr = Array.from({ length: arr.length }, () => false);
+
+  function DFS(L) {
+    if (L === arr.length) {
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+        if (tmparr[i]) {
+          sum += arr[i];
+        }
+      }
+      if (sum <= c) {
+        answer = Math.max(answer, sum);
+      }
+    } else {
+      tmparr[L] = true;
+      DFS(L + 1);
+      tmparr[L] = false;
+      DFS(L + 1);
+    }
+  }
+
+  DFS(0);
   return answer;
 }
 
 let arr = [81, 58, 42, 33, 61];
 console.log(solution(259, arr));
+
+// 바둑이 승차(DFS)
+function solution2(c, arr) {
+  let answer = 0;
+  function DFS(L, sum) {
+    if (L === arr.length) {
+      if (sum > c) return;
+      answer = Math.max(answer, sum);
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+
+  DFS(0, 0);
+  return answer;
+}
+
+let arr2 = [81, 58, 42, 33, 61];
+console.log("solution2 = " + solution2(259, arr));

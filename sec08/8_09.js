b92f38eb052c067c54f3e47b4c17e1f98a321fe7
Jang Oo Bin <jwb449730@naver.com>
2021-11-17 16:23:16 +0900

    2021_11_17

--- sec08/8_09.js
+++ sec08/8_09.js
@@ -1,6 +1,21 @@
 // 동전교환
 function solution(m, arr) {
-  let answer = 0;
+  let answer = Number.MAX_SAFE_INTEGER;
+
+  function DFS(L, sum) {
+    if (sum >= m) {
+      if (sum > m) {
+        sum = 0;
+      } else {
+        answer = Math.min(answer, L);
+      }
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        DFS(L + 1, sum + arr[i]);
+      }
+    }
+  }
+  DFS(0, 0);
   return answer;
 }
 

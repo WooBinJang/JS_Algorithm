5b98c795ff34ff6d48bf0241bdbb994107044295
Jang Oo Bin <jwb449730@naver.com>
2021-11-19 16:36:16 +0900

    2021_11_19

--- sec08/8_10.js
+++ sec08/8_10.js
@@ -1,8 +1,25 @@
 // 순열 구하기
 function solution(m, arr) {
   let answer = [];
-  function DFS(L) {}
+  let checkArr = Array.from({ length: arr.length }, () => false);
+  let tmpArr = Array.from({ length: m }, () => false);
+  function DFS(L) {
+    if (L === m) {
+      answer.push(tmpArr.slice());
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        if (!checkArr[i]) {
+          checkArr[i] = true;
+          tmpArr[L] = arr[i];
+          DFS(L + 1);
+          checkArr[i] = false;
+        }
+      }
+    }
+  }
   DFS(0);
+  console.log(answer.length);
+
   return answer;
 }
 
--- sec08/8_11.js
+++ sec08/8_11.js
@@ -1,8 +1,33 @@
 // 팩토리얼
 function solution(n) {
-  let answer;
-
+  let answer = 0;
+  function DFS(L, multiply) {
+    if (L === 0) {
+      answer += multiply;
+      return;
+    } else {
+      DFS(L - 1, multiply * L);
+    }
+  }
+  DFS(n, 1);
   return answer;
 }
 
 console.log(solution(5));
+
+// 팩토리얼
+function solution2(n) {
+  let answer;
+  function DFS(n) {
+    if (n === 1) {
+      return 1;
+    } else {
+      return n * DFS(n - 1);
+    }
+  }
+
+  answer = DFS(n);
+  return answer;
+}
+
+console.log("solution2 = " + solution2(5));

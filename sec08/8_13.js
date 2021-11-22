94b521a8c24cd2574fb13936c7804e520d33d304
Jang Oo Bin <jwb449730@naver.com>
2021-11-22 19:37:20 +0900

    2021_11_22

--- sec08/8_13.js
+++ sec08/8_13.js
@@ -1,6 +1,43 @@
 // 수열 추측하기
 function solution(n, f) {
   let answer;
+  let flag = 0;
+  let dy = Array.from(Array(11), () => Array(11).fill(0)); // 메모이제이션
+  let ch = Array.from({ length: n + 1 }, () => 0); // 체크 배열
+  let p = Array.from({ length: n }, () => 0); //
+  let b = Array.from({ length: n }, () => 0); //
+
+  function Combi(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) {
+      return 1;
+    } else {
+      return (dy[n][r] = Combi(n - 1, r - 1) + Combi(n - 1, r));
+    }
+  }
+
+  function DFS(L, sum) {
+    if (flag) return;
+    if (L === n && sum === f) {
+      answer = p.slice();
+      flag = 1;
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          p[L] = i;
+          DFS(L + 1, sum + b[L] * p[L]);
+          ch[i] = 0;
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    b[i] = Combi(n - 1, i);
+  }
+
+  DFS(0, 0);
   return answer;
 }
 

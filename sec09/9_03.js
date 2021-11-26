0cd1bf0ec3daa248425fd4112bd12d7d7801348c
Jang Oo Bin <jwb449730@naver.com>
2021-11-26 16:18:56 +0900

    2021_11_26

--- sec09/9_03.js
+++ sec09/9_03.js
@@ -1,7 +1,33 @@
 // 경로 탐색(인접리스트)
 function solution(n, arr) {
   let answer = 0;
+  let graph = Array.from(Array(n + 1), () => Array());
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  let tmp = [];
+  for (let [a, b] of arr) {
+    graph[a].push(b);
+  }
 
+  function DFS(v) {
+    if (v === n) {
+      console.log(tmp);
+      answer++;
+    } else {
+      for (let i = 0; i <= graph[v].length; i++) {
+        if (ch[graph[v][i]] === 0) {
+          tmp.push(graph[v][i]);
+          ch[graph[v][i]] = 1;
+          DFS(graph[v][i]);
+          tmp.pop();
+          ch[graph[v][i]] = 0;
+        }
+      }
+    }
+  }
+
+  tmp[0] = 1;
+  ch[1] = 1;
+  DFS(1);
   return answer;
 }
 

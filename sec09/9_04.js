0ae4e903d5021aea24d1f63d732d5bf44ed70019
Jang Oo Bin <jwb449730@naver.com>
2021-11-30 16:40:43 +0900

    2021_11_30

--- sec09/9_04.js
+++ sec09/9_04.js
@@ -3,9 +3,11 @@ function solution(board) {
   let answer = 0;
   let dx = [1, 0, -1, 0];
   let dy = [0, 1, 0, -1];
-  let ch = Array.from({ length: board.length + 1 }, () => 0);
+  //[1,0] 오 [0,1] 아래 [-1,0] 왼 [0,-1] 위
+
   function DFS(x, y) {
     if (x === board.length - 1 && y === board.length - 1) {
+      //도착좌표
       answer++;
     } else {
       for (let i = 0; i < dx.length; i++) {
@@ -20,7 +22,7 @@ function solution(board) {
         ) {
           board[nx][ny] = 1; // 왔던 좌표 체크
           DFS(nx, ny);
-          board[nx][ny] = 0;
+          board[nx][ny] = 0; // 왔던 좌표 체크 해제
         }
       }
     }
@@ -40,5 +42,4 @@ let arr = [
   [1, 1, 0, 1, 1, 0, 0],
   [1, 0, 0, 0, 0, 0, 0],
 ];
-
 console.log(solution(arr));
--- sec09/9_05.js
+++ sec09/9_05.js
@@ -1,6 +1,16 @@
 function solution() {
   let answer = "";
+  let queue = [];
+  queue.push(1);
 
+  while (queue.length !== 0) {
+    let x = queue.shift();
+    answer += x + " ";
+    for (let nv of [x * 2, x * 2 + 1]) {
+      if (nv > 7) continue;
+      queue.push(nv);
+    }
+  }
   return answer;
 }
 

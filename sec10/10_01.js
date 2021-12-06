68f32a2beb358a51977544012d1e904ac588d7db
Jang Oo Bin <jwb449730@naver.com>
2021-12-06 15:49:17 +0900

    2021_12_06

--- sec10/10_01.js
+++ sec10/10_01.js
@@ -1,7 +1,13 @@
 //계단오르기
 function solution(n) {
   let answer = 0;
-
+  let dy = Array.from({ length: n + 1 }, () => 0);
+  dy[1] = 1;
+  dy[2] = 2;
+  for (let i = 3; i < dy.length; i++) {
+    dy[i] = dy[i - 1] + dy[i - 2];
+  }
+  answer = dy[n];
   return answer;
 }
 

1a4af55b32b9db475d2c1b15c26afcf59e8ae341
Jang Oo Bin <jwb449730@naver.com>
2021-12-09 16:18:30 +0900

    2021_12_09

--- sec10/10_02.js
+++ sec10/10_02.js
@@ -1,7 +1,13 @@
 //돌다리 건너기
 function solution(n) {
   let answer = 0;
-  let a =1;
+  let arr = Array.from({ length: n + 2 }, () => 0);
+  arr[1] = 1;
+  arr[2] = 2;
+  for (let i = 3; i < arr.length; i++) {
+    arr[i] = arr[i - 2] + arr[i - 1];
+  }
+  answer = arr[arr.length - 1];
   return answer;
 }
 

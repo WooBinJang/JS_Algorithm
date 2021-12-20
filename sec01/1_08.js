cd90fdf77b0983a3e63e284f3354332cc9f791f4
Jang Oo Bin <jwb449730@naver.com>
2021-12-20 17:21:02 +0900

    중복문자제거 다시풀기

--- sec01/1_08.js
+++ sec01/1_08.js
@@ -1,25 +1,19 @@
 //일곱 난쟁이 (다시풀기)
 function solution(arr) {
-  let sum = -100;
+  let sum = 0;
   for (let x of arr) {
     sum += x;
   }
-
-  arr.sort(function (a, b) {
-    return a - b;
-  });
-
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === sum) {
-        arr.splice(i, 1); // index i번째 1개 제거
-        arr.splice(j - 1, 1);
+      if (sum - (arr[i] + arr[j]) === 100) {
+        arr.splice(arr.indexOf(arr[i]), 1);
+        arr.splice(arr.indexOf(arr[j]), 1);
       }
     }
   }
-  answer = arr;
-  return answer;
+  return arr;
 }
 
-let arr = [20, 7, 23, 19, 10, 99, 22, 8, 13];
+let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
 console.log(solution(arr));
--- sec01/1_16.js
+++ sec01/1_16.js
@@ -28,3 +28,41 @@ function solution(s) {
   return answer;
 }
 console.log(solution("ksekkset"));
+
+/*
+소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램을 작성하
+세요.
+제거된 문자열의 각 문자는 원래 문자열의 순서를 유지합니다.
+▣ 입력설명
+첫 줄에 문자열이 입력됩니다.
+▣ 출력설명
+첫 줄에 중복문자가 제거된 문자열을 출력합니다.
+▣ 입력예제 1 
+ksekkset
+▣ 출력예제 1
+kset
+
+*/
+
+function solution3(s) {
+  let tmpArr = [];
+  let answer = "";
+  for (let i = 0; i < s.length; i++) {
+    tmpArr.push(s[i]);
+  }
+  for (let i = 0; i < s.length - 1; i++) {
+    for (let j = i + 1; j < s.length; j++) {
+      if (tmpArr[i] === tmpArr[j]) {
+        tmpArr[j] = "#";
+      }
+    }
+  }
+  for (let x of tmpArr) {
+    if (x !== "#") {
+      answer += x;
+    }
+  }
+
+  return answer;
+}
+console.log("solution3= " + solution3("ksekkset"));
